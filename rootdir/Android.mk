9f32b04b6b98699c79eb4982ca47881038e7553c
xyzuniverse <dev.xyzuniverse@gmail.com>
2023-03-01 00:22:54 +0000

    pissarro: Another dirty commit * Move-out disable hw overlay into system.prop and init * Import init.mt6877.rc from stock n copy-out into vendor-overlay
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -17,11 +17,19 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.mediatek.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.mediatek.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mt6877.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.mt6877.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+LOCAL_MODULE_PATH  := $(TARGET_OUT_PRODUCT)/vendor_overlay/30/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
